43c16b9add2f93a3cca87aeb238c7fe65149ab2a
bndlfm <benjamindanielseddonflaim@gmail.com>
2023-09-14 22:45:05 -0500

    removed fzf-lua.lua and fzf.lua

--- lua/plugins/fzf-lua.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ibhagwan/fzf-lua",
-  enabled = true,
-  keys = {
-    vim.keymap.set("", "<C-p>", ":FzfLua files<CR>", { desc = "-show files-" }),
-    vim.keymap.set("", "<C-f>", ":Rg<CR>", { desc = "ripgrep" }),
-  },
-}
